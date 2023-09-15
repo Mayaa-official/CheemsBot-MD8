d8776fd71ddd7c72436712bcdbc82b9fabd8b5f6
Mayaa-official <144865350+Mayaa-official@users.noreply.github.com>
2023-09-15 08:38:43 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,11 +2,11 @@ const chalk = require("chalk")
 const fs = require("fs")
 
 //aumto presence update
-global.autoTyping = false //auto tying in gc (true to on, false to off)
-global.autoRecord = false //auto recording (true to on, false to off)
+global.autoTyping = true //auto tying in gc (true to on, false to off)
+global.autoRecord = true //auto recording (true to on, false to off)
 global.autoblockmorroco = false //auto block 212 (true to on, false to off)
 global.autokickmorroco = false //auto kick 212 (true to on, false to off) 
-global.antispam = false //auto kick spammer (true to on, false to off)
+global.antispam = true //auto kick spammer (true to on, false to off)
 
 
 //if api key expire, u can generate one from here: https://beta.openai.com/account/api-keys
@@ -21,27 +21,27 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['94768480793'] //ur owner number
+global.ownernomer = "94785760531" //ur owner number2
+global.ownername = "Samith dilshan" //ur owner name
+global.ytname = "Group link" //ur yt chanel name
+global.socialm = "GitHub: Mayaa" //ur github or insta name
+global.location = "Sri lanka,nildandahinna" //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
+global.botname = "𝚳𝚫𝐘𝚫 𝚩𝚯𝚻 𝚳𝐃"
+global.ownernumber = '94768480793'
+global.ownername = 'samith dilshan'
+global.ownerNumber = ["94768480793@s.whatsapp.net"]
 global.ownerweb = "https://youtube.com/@DGXeon"
 global.websitex = "https://youtu.be/L_SIk59QeAU"
 global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
-global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
+global.themeemoji = '💥'
+global.wm = "𝚳𝚫𝐘𝚫 𝚩𝚯𝚻 𝚳𝐃"
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
 global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n"
-global.creator = "916909137213@s.whatsapp.net"
+global.author = "𝚳𝚫𝐘𝚫 𝚩𝚯𝚻 𝚳𝐃\n\n"
+global.creator = "94768480793@s.whatsapp.net"
 global.prefa = ['','!','.','#','&']
 global.sessionName = 'session'
 global.hituet = 0
